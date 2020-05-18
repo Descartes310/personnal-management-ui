f1cd5a21da4acb1d13ccd4a5f0dbb42cef74679c
franklintanemou@gmail.com <everlote123>
2020-05-18 11:51:41 +0100

    init

---
+++ src/app/Routes.ts
@@ -0,0 +1,15 @@
+export const SERVER = 'http://localhost:8000/api/';
+
+export const LOGIN = SERVER+'auth/token';
+export const USERS = SERVER+'users';
+export const CHAT = SERVER+'chats';
+export const DOCUMENT = SERVER + 'documents';
+export const ROLE = SERVER + 'roles';
+export const assignment_type = SERVER + 'assignment_types';
+export const PERMISSION = SERVER + 'permissions';
+export const PRO_SITUATION = SERVER + 'pro_situations';
+export const CONTACT = SERVER + 'contacts';
+export const VACATIONTYPE = SERVER + 'vacation_types';
+export const LICENSE = SERVER + 'licenses';
+export const LICENSE_TYPE = SERVER + 'license_types';
+export const LICENSETYPE = SERVER +'license_types';
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,63 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, CanActivate } from '@angular/router';
+import { AuthGuardService as AuthGuard } from './_guards/auth.guard'
+
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { DocumentViewerComponent } from './document-viewer/document-viewer.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { AddProSituationComponent } from './pro_situations/add-pro-situation/add-pro-situation.component'
+import { AddRoleComponent } from './roles/add-role/add-role.component';
+import { AllRolesComponent } from './roles/all-roles/all-roles.component';
+import { UpdateRoleComponent } from './roles/update-role/update-role.component';
+import { DetailsRoleComponent } from './roles/details-role/details-role.component';
+import { UpdateProSituationComponent } from './pro_situations/update-pro-situation/update-pro-situation.component';
+import { ChatComponent } from './chat/chat.component'
+import { AddContactComponent } from './contacts/add-contact/add-contact.component';
+import { UpdateContactComponent } from './contacts/update-contact/update-contact.component';
+import { AddAssignmentTypeComponent } from './assignment-types/add-assignment-type/add-assignment-type.component';
+import { UpdateAssignmentTypeComponent } from './assignment-types/update-assignment-type/update-assignment-type.component';
+import { AddProfileComponent } from './profiles/add-profile/add-profile.component';
+import { UpdateProfileComponent } from './profiles/update-profile/update-profile.component';
+
+import { AddVacationTypeComponent } from './vacation_types/add-vacation-type/add-vacation-type.component';
+import { UpdateVacationTypeComponent } from './vacation_types/update-vacation-type/update-vacation-type.component';
+import { AddLicenseComponent } from './licenses/add-license/add-license.component';
+import { UpdateLicenseComponent } from './licenses/update-license/update-license.component';
+import { AddLicensetypesComponent } from './licensetypes/add-licensetypes/add-licensetypes.component';
+import { UpdateLicensetypesComponent } from './licensetypes/update-licensetypes/update-licensetypes.component';
+
+
+//const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'chat', component: ChatComponent },
+  { path: 'pro-situations/add', component: AddProSituationComponent },
+  { path: 'pro-situations/update/:id', component: UpdateProSituationComponent },
+  { path: 'profiles/add', component: AddProfileComponent },
+  { path: 'profiles/update/:id', component: UpdateProfileComponent },
+  { path: 'licensetypes/add', component: AddLicensetypesComponent },
+  { path: 'licensetypes/update/:id', component: UpdateLicensetypesComponent },
+  { path: 'roles/add', component: AddRoleComponent },
+  { path: 'roles/all', component: AllRolesComponent },
+  { path: 'roles/update/:id', component: UpdateRoleComponent },
+  { path: 'roles/details/:id', component: DetailsRoleComponent },
+  { path: 'vacation-types/add', component: AddVacationTypeComponent },
+  { path: 'vacation-types/update/:id', component: UpdateVacationTypeComponent },
+  { path: 'licenses/add', component: AddLicenseComponent },
+  { path: 'licenses/update/:id', component: UpdateLicenseComponent },
+  { path: 'document-viewer', component: DocumentViewerComponent },
+  //routes pour ajout et update des contacts
+  { path: 'contacts/update/:id', component:UpdateContactComponent},
+  { path: 'contacts/add',component:AddContactComponent},
+  { path: 'assignment-types/add', component: AddAssignmentTypeComponent },
+  { path: 'assignment-types/update/:id', component: UpdateAssignmentTypeComponent },
+  //{ path: '404', component: NotfoundComponent },
+  { path: '**', redirectTo: 'login' },
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
