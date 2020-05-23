b92293d9ce19518ff6b96a0a5ec6f73b4909f4d4
Nganji Nya Armel Boris <armelnganji@yahoo.com>
2020-05-23 08:57:58 +0200

    complete task

--- src/app/_models/assignment.model.ts
+++ src/app/_models/assignment.model.ts
@@ -4,6 +4,8 @@ import { AssignmentType } from './assignment-type.model';
 export class Assignment extends Super<Assignment> {
 
     public user_id: number;
+    public user?: any;
+    public assignment_type?: AssignmentType;
     public assignment_type_id: number;
     public destination:string;
     public signature_date:Date;
