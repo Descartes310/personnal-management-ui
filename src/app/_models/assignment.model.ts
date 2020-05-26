08bc96e4eec663a25a458abc06c58c98af2a0331
Descartes Fowo <descartesouambo@gmail.com>
2020-05-26 10:57:27 +0100

    Merge with Cassandra

--- src/app/_models/assignment.model.ts
+++ src/app/_models/assignment.model.ts
@@ -1,11 +1,11 @@
 import { Super } from './super.model';
-import { Assignmenttype } from './assignmenttype.model';
+import { AssignmentType } from './assignmenttype.model';
 
 export class Assignment extends Super<Assignment> {
 
     public user_id: number;
     public user?: any;
-    public assignment_type?: Assignmenttype;
+    public assignment_type?: AssignmentType;
     public assignment_type_id: number;
     public destination:string;
     public signature_date:Date;
--- src/app/assignment-types/add-assignment-type/add-assignment-type.component.ts
+++ src/app/assignment-types/add-assignment-type/add-assignment-type.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { AssignmenttypeService } from 'src/app/_services/assignmenttype.service';
+import { AssignmentTypeService } from 'src/app/_services/assignmenttype.service';
 import { NotifService } from 'src/app/_services/notif.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
-import { Assignmenttype } from 'src/app/_models/assignmenttype.model';
+import { AssignmentType } from 'src/app/_models/assignmenttype.model';
 import { Router } from '@angular/router';
 
 @Component({
@@ -23,7 +23,7 @@ export class AddAssignmentTypeComponent implements OnInit {
   role_name = '';
 
   constructor(
-    private assignmentTypeServie: AssignmenttypeService,
+    private assignmentTypeServie: AssignmentTypeService,
     private notifService: NotifService,
     private formBuilder: FormBuilder,
     private translate: TranslateService,
