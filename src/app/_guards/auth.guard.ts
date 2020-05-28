13ad94120c568c3e176b9ee15c62ca63e1a8e735
Ben677 <meztsacar@gmail.com>
2020-05-28 19:02:50 +0100

    add permission to routes and remove unused files

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -1,42 +1,42 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../_services/auth.service';
+import { NotifService } from '../_services/notif.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
 
-  roles;
-  userRoles: any[];
-  authorized: boolean = false;
-
   constructor(
-    private authService: AuthService, 
+    private authService: AuthService,
+    private notifService: NotifService,
+    private translate: TranslateService,
     private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot): boolean {
 
-    let isLogin = route.data.isLoginRoute ? route.data.isLoginRoute : false;
-    this.roles = route.data.roles;
-    this.userRoles = this.authService.getRoles();
+    const isLogin = route.data.isLoginRoute ? route.data.isLoginRoute : false;
+    const permissions: string[] = route.data.permissions;
 
-    if(isLogin && !this.authService.isAuthenticated()) {
-      return true
+    if (isLogin && !this.authService.isAuthenticated()) {
+      return true;
     }
-    
+
     if (!this.authService.isAuthenticated()) {
       this.router.navigate(['login']);
-    } 
-    
-    if(isLogin && this.authService.isAuthenticated()) {
+    }
+
+    if (isLogin && this.authService.isAuthenticated()) {
       this.router.navigate(['home']);
     }
 
-    if(this.roles) {
-      let data = this.authService.hasPermission(this.roles);
-      if(data) {
+    if (permissions) {
+      const hasPermission = this.authService.hasPermission(permissions);
+      if (hasPermission) {
         return true;
       } else {
-        this.router.navigate(['home']);
+        this.translate.get('HTTP_ERROR_MSG.403').subscribe(val => this.notifService.warning(val));
+        // this.router.navigate(['home']);
         return false;
       }
     } else {
